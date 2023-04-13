a488ec0a957e5d0cd3b1dd72dde1ca9425a83805
Leantar <t.peters@mailbox.org>
2023-04-13 14:33:19 +0200

    change auth from cookie to bearer

--- server/middleware/authentication.go
+++ server/middleware/authentication.go
@@ -14,21 +14,17 @@ import (
 func CheckAuthentication() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			sessionCookie, err := c.Request().Cookie("session")
-			if err != nil {
+			bearer := c.Request().Header.Get("Authorization")
+			if len(bearer) < 8 {
 				return echo.NewHTTPError(http.StatusUnauthorized, "Missing or invalid session")
 			}
-
-			idCookie, err := c.Request().Cookie("user_email")
-			if err != nil {
-				return echo.NewHTTPError(http.StatusUnauthorized, "Missing or invalid session")
-			}
+			email := c.Request().Header.Get("X-Email")
 
 			tx := c.Get("tx").(common.Transaction)
 
-			user, err := tx.Users().GetUserByEmail(idCookie.Value, c.Request().Context())
+			user, err := tx.Users().GetUserByEmail(email, c.Request().Context())
 			if errors.Is(err, common.ErrNotFound) {
-				return echo.NewHTTPError(http.StatusNotFound, "User unknown")
+				return echo.NewHTTPError(http.StatusUnauthorized, "Missing or invalid session")
 			}
 			if err != nil {
 				return fmt.Errorf("failed to get user by id: %w", err)
@@ -39,7 +35,7 @@ func CheckAuthentication() echo.MiddlewareFunc {
 				return fmt.Errorf("failed to decode verification key: %w", err)
 			}
 
-			if !validateJWT(sessionCookie.Value, pk) {
+			if !validateJWT(bearer[7:], pk) {
 				return echo.NewHTTPError(http.StatusUnauthorized, "Missing or invalid session")
 			}
 
@@ -54,6 +50,7 @@ func validateJWT(tokenString string, pk ed25519.PublicKey) bool {
 	parser := jwt.NewParser(
 		jwt.WithIssuedAt(),
 		jwt.WithValidMethods([]string{jwt.SigningMethodEdDSA.Alg()}),
+		jwt.WithAudience("backend"),
 	)
 
 	_, err := parser.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
